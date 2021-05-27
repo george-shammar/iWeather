8724461886603a0e1bd232f5bc22b97f1bbe1cdf
george-shammar <george.gbenle@outlook.com>
2021-05-27 21:20:33 +0000

    Fetch data info from openWeather

--- src/get_data.js
+++ src/get_data.js
@@ -1,6 +1,9 @@
 
 const dataInfo = () => {
-    
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c7911001fbb406aa98e26c70ab98ec78', {mode: 'cors'})
+    .then(function(response) {
+      console.log(response.json());
+    });
 }
 
 
