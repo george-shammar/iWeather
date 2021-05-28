4f074a063a778d06798f970a7dd7a175599c024d
george-shammar <george.gbenle@outlook.com>
2021-05-28 18:24:37 +0000

    Create function to retrieve location from users

--- src/get_data.js
+++ src/get_data.js
@@ -1,9 +1,10 @@
-const getLocation = () => {
-    
+const getLocation = (location) => {
+    location = document.getElementById('location').value;
+    return location;
 }
 
 const dataInfo = () => {
-    let location = ""
+    getLocation(location);
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=c7911001fbb406aa98e26c70ab98ec78`, {mode: 'cors'})
     .then(function(response) {
       console.log(response.json());
