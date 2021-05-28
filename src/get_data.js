485b632ae59a18fc69b8f5eb892b4156eefe2792
george-shammar <george.gbenle@outlook.com>
2021-05-28 18:09:00 +0000

    Add form

--- src/get_data.js
+++ src/get_data.js
@@ -1,21 +1,19 @@
 const getLocation = () => {
-
+    
 }
 
 const dataInfo = () => {
-    let location = "Auckland"
+    let location = ""
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=c7911001fbb406aa98e26c70ab98ec78`, {mode: 'cors'})
     .then(function(response) {
       console.log(response.json());
     });
 }
 
-
 const getData = async () => {
     try {
         const weather = await dataInfo();
         console.log(weather);
-        // dataInfo();
     } catch (err) {
         console.log(err);
     }
