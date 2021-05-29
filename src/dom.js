f5dd39400de5e3d105975880cb53d853a89d7330
george-shammar <george.gbenle@outlook.com>
2021-05-29 18:11:13 +0000

    Fix eslint linter errors

--- src/dom.js
+++ src/dom.js
@@ -1,157 +1,152 @@
 
-const weatherCard = (mainTemp, cityName, countryName, lowTemp, highTemp, humidity, pressure, description) => {
-    document.querySelector('#body').innerHTML = '';
-    // card Container
-    const cardContainer = document.createElement('div');
-    cardContainer.setAttribute('class', 'weather-card mt-5 border shadow');
+const weatherCard = (mainTemp, cityName, countryName, lowTemp, highTemp,
+  humidity, pressure, description) => {
+  document.querySelector('#body').innerHTML = '';
+  // card Container
+  const cardContainer = document.createElement('div');
+  cardContainer.setAttribute('class', 'weather-card mt-5 border shadow');
 
-    const body = document.getElementById('body');
-    body.appendChild(cardContainer);
+  const body = document.getElementById('body');
+  body.appendChild(cardContainer);
 
-    // cloud icon
-    const cloudIcon = document.createElement('i');
-    cloudIcon.setAttribute('class', 'fas fa-cloud-sun-rain cloud');
-    cardContainer.appendChild(cloudIcon);
+  // cloud icon
+  const cloudIcon = document.createElement('i');
+  cloudIcon.setAttribute('class', 'fas fa-cloud-sun-rain cloud');
+  cardContainer.appendChild(cloudIcon);
 
-    // data Container
-    const dataContainer = document.createElement('div');
-    dataContainer.setAttribute('class', 'd-flex justify-content-center color-font');
-    cardContainer.appendChild(dataContainer);
+  // data Container
+  const dataContainer = document.createElement('div');
+  dataContainer.setAttribute('class', 'd-flex justify-content-center color-font');
+  cardContainer.appendChild(dataContainer);
 
-    // max temperature conatainer
-    const maxTempContainer = document.createElement('div');
+  // max temperature conatainer
+  const maxTempContainer = document.createElement('div');
 
-    const maxTemp = document.createElement('h4');
-    maxTemp.setAttribute('class', 'px-3 fw-bold');
-    const maxTempText =  document.createTextNode("Max-Temp");
-    maxTemp.appendChild(maxTempText);
+  const maxTemp = document.createElement('h4');
+  maxTemp.setAttribute('class', 'px-3 fw-bold');
+  const maxTempText = document.createTextNode('Max-Temp');
+  maxTemp.appendChild(maxTempText);
 
-    const maxTempValue = document.createElement('h4');
-    maxTempValue.setAttribute('class', 'px-5 fw-bold');
-    const maxValue =  document.createTextNode(`${highTemp}°`);
-    maxTempValue.appendChild(maxValue);
+  const maxTempValue = document.createElement('h4');
+  maxTempValue.setAttribute('class', 'px-5 fw-bold');
+  const maxValue = document.createTextNode(`${highTemp}°`);
+  maxTempValue.appendChild(maxValue);
 
-    maxTempContainer.appendChild(maxTemp);
-    maxTempContainer.appendChild(maxTempValue);
+  maxTempContainer.appendChild(maxTemp);
+  maxTempContainer.appendChild(maxTempValue);
 
-    dataContainer.appendChild(maxTempContainer);
+  dataContainer.appendChild(maxTempContainer);
 
 
-    // min temperature conatainer
-    const minTempContainer = document.createElement('div');
+  // min temperature conatainer
+  const minTempContainer = document.createElement('div');
 
-    const minTemp = document.createElement('h4');
-    minTemp.setAttribute('class', 'px-3 fw-bold');
-    const minTempText =  document.createTextNode("Min-Temp");
-    minTemp.appendChild(minTempText);
+  const minTemp = document.createElement('h4');
+  minTemp.setAttribute('class', 'px-3 fw-bold');
+  const minTempText = document.createTextNode('Min-Temp');
+  minTemp.appendChild(minTempText);
 
-    const minTempValue = document.createElement('h4');
-    minTempValue.setAttribute('class', 'px-5 fw-bold');
-    const minValue =  document.createTextNode(`${lowTemp}°`);
-    minTempValue.appendChild(minValue);
+  const minTempValue = document.createElement('h4');
+  minTempValue.setAttribute('class', 'px-5 fw-bold');
+  const minValue = document.createTextNode(`${lowTemp}°`);
+  minTempValue.appendChild(minValue);
 
-    minTempContainer.appendChild(minTemp);
-    minTempContainer.appendChild(minTempValue);
+  minTempContainer.appendChild(minTemp);
+  minTempContainer.appendChild(minTempValue);
 
-    dataContainer.appendChild(minTempContainer);
+  dataContainer.appendChild(minTempContainer);
 
-    // humidity conatainer
-    const humidityContainer = document.createElement('div');
+  // humidity conatainer
+  const humidityContainer = document.createElement('div');
 
-    const minHumidity = document.createElement('h4');
-    minHumidity.setAttribute('class', 'px-3 fw-bold');
-    const minHumidityText =  document.createTextNode("Humidity");
-    minHumidity.appendChild(minHumidityText);
+  const minHumidity = document.createElement('h4');
+  minHumidity.setAttribute('class', 'px-3 fw-bold');
+  const minHumidityText = document.createTextNode('Humidity');
+  minHumidity.appendChild(minHumidityText);
 
-    const minHumidityValue = document.createElement('h4');
-    minHumidityValue.setAttribute('class', 'px-5 fw-bold');
-    const humidityValue =  document.createTextNode(`${humidity}%`);
-    minHumidityValue.appendChild(humidityValue);
+  const minHumidityValue = document.createElement('h4');
+  minHumidityValue.setAttribute('class', 'px-5 fw-bold');
+  const humidityValue = document.createTextNode(`${humidity}%`);
+  minHumidityValue.appendChild(humidityValue);
 
-    humidityContainer.appendChild(minHumidity);
-    humidityContainer.appendChild(minHumidityValue);
+  humidityContainer.appendChild(minHumidity);
+  humidityContainer.appendChild(minHumidityValue);
 
-    dataContainer.appendChild(humidityContainer);
+  dataContainer.appendChild(humidityContainer);
 
-    // pressure conatainer
-    const pressureContainer = document.createElement('div');
+  // pressure conatainer
+  const pressureContainer = document.createElement('div');
 
-    const minPressure = document.createElement('h4');
-    minPressure.setAttribute('class', 'px-3 fw-bold');
-    const minPressureText =  document.createTextNode("Pressure");
-    minPressure.appendChild(minPressureText);
+  const minPressure = document.createElement('h4');
+  minPressure.setAttribute('class', 'px-3 fw-bold');
+  const minPressureText = document.createTextNode('Pressure');
+  minPressure.appendChild(minPressureText);
 
-    const minPressureValue = document.createElement('h4');
-    minPressureValue.setAttribute('class', 'px-5 fw-bold');
-    const pressureValue =  document.createTextNode(`${pressure}ha`);
-    minPressureValue.appendChild(pressureValue);
+  const minPressureValue = document.createElement('h4');
+  minPressureValue.setAttribute('class', 'px-5 fw-bold');
+  const pressureValue = document.createTextNode(`${pressure}ha`);
+  minPressureValue.appendChild(pressureValue);
 
-    pressureContainer.appendChild(minPressure);
-    pressureContainer.appendChild(minPressureValue);
+  pressureContainer.appendChild(minPressure);
+  pressureContainer.appendChild(minPressureValue);
 
-    dataContainer.appendChild(pressureContainer);
+  dataContainer.appendChild(pressureContainer);
 
-    // city container
-    const cityContainer = document.createElement('div');
-    cityContainer.setAttribute('class', 'd-flex justify-content-center');
-    cardContainer.appendChild(cityContainer);
+  // city container
+  const cityContainer = document.createElement('div');
+  cityContainer.setAttribute('class', 'd-flex justify-content-center');
+  cardContainer.appendChild(cityContainer);
 
-    // main city
-    const mainCity = document.createElement('div');
-    mainCity.setAttribute('class', 'pe-5');
-    cityContainer.appendChild(mainCity);
+  // main city
+  const mainCity = document.createElement('div');
+  mainCity.setAttribute('class', 'pe-5');
+  cityContainer.appendChild(mainCity);
 
-    // value city
-    const valueCityparagraph = document.createElement('p');
-    valueCityparagraph.setAttribute('class', 'main color-white');
-    const valueCity = document.createTextNode(`${mainTemp}°`);
-    valueCityparagraph.appendChild(valueCity);
-    mainCity.appendChild(valueCityparagraph);
+  // value city
+  const valueCityparagraph = document.createElement('p');
+  valueCityparagraph.setAttribute('class', 'main color-white');
+  const valueCity = document.createTextNode(`${mainTemp}°`);
+  valueCityparagraph.appendChild(valueCity);
+  mainCity.appendChild(valueCityparagraph);
 
-    // city paragraph
-    const cityparagraph = document.createElement('p');
-    cityparagraph.setAttribute('class', 'fs-3 color-white');
-    const city = document.createTextNode(`${cityName}, ${countryName}`);
-    cityparagraph.appendChild(city);
-    mainCity.appendChild(cityparagraph);
+  // city paragraph
+  const cityparagraph = document.createElement('p');
+  cityparagraph.setAttribute('class', 'fs-3 color-white');
+  const city = document.createTextNode(`${cityName}, ${countryName}`);
+  cityparagraph.appendChild(city);
+  mainCity.appendChild(cityparagraph);
 
-    // line
-    const line = document.createElement('div');
-    line.setAttribute('class', 'line');
-    cityContainer.appendChild(line);
+  // line
+  const line = document.createElement('div');
+  line.setAttribute('class', 'line');
+  cityContainer.appendChild(line);
 
-    // clouds
-    const cloudContainer = document.createElement('div');
-    
+  // clouds
+  const cloudContainer = document.createElement('div');
 
-    const cloudParagraph = document.createElement('p');
-    cloudParagraph.setAttribute('class', 'main-desc ps-5 color-white');
-    const cloud = document.createTextNode(description);
-    cloudParagraph.appendChild(cloud);
-    cloudContainer.appendChild(cloudParagraph);
 
-    // celsius button
-    const buttonDiv = document.createElement('div');
-    buttonDiv.setAttribute('class', 'd-flex mx-5');
+  const cloudParagraph = document.createElement('p');
+  cloudParagraph.setAttribute('class', 'main-desc ps-5 color-white');
+  const cloud = document.createTextNode(description);
+  cloudParagraph.appendChild(cloud);
+  cloudContainer.appendChild(cloudParagraph);
 
-    const celsiusButton = document.createElement('button');
-    celsiusButton.setAttribute('id', 'cel');
-    const celText = document.createTextNode('°C');
-    celsiusButton.appendChild(celText);
-    buttonDiv.appendChild(celsiusButton)
+  // celsius button
+  const buttonDiv = document.createElement('div');
+  buttonDiv.setAttribute('class', 'd-flex mx-5');
 
-    // ferheihet button
-    const ferButton = document.createElement('button');
-    ferButton.setAttribute('id', 'fer');
-    const ferText = document.createTextNode('°F');
-    ferButton.appendChild(ferText);
-    buttonDiv.appendChild(ferButton)
 
-    cloudContainer.appendChild(buttonDiv);
+  // ferheihet button
+  const ferButton = document.createElement('button');
+  ferButton.setAttribute('id', 'fer');
+  const ferText = document.createTextNode('°F');
+  ferButton.appendChild(ferText);
+  buttonDiv.appendChild(ferButton);
 
-    cityContainer.appendChild(cloudContainer);
-}
+  cloudContainer.appendChild(buttonDiv);
 
+  cityContainer.appendChild(cloudContainer);
+};
 
 
 export default weatherCard;
--- src/get_data.js
+++ src/get_data.js
@@ -1,51 +1,47 @@
 import weatherCard from './dom';
 
 const dataInfo = (location) => {
-    const submitButton = document.getElementById('submit');
-    
-    submitButton.onclick = () => {
-       location = document.getElementById('location').value;
-        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=c7911001fbb406aa98e26c70ab98ec78`, {mode: 'cors'})
-        .then(function(response) {
-          return (response.json());
-        })
-        .then(function(response) {
-            const description = response.weather[0].description;
-            const mainTemp = Math.round((response.main.temp)-273);
-            const cityName = response.name;
-            const countryName = response.sys.country;
-            const humidity = response.main.humidity;
-            const pressure = response.main.pressure;
-            const lowTemp = Math.round((response.main.temp_min)-273);
-            const highTemp = Math.round((response.main.temp_max)-273);
+  const submitButton = document.getElementById('submit');
 
-            weatherCard(mainTemp, cityName, countryName, lowTemp, highTemp, humidity, pressure, description);
+  submitButton.onclick = () => {
+    location = document.getElementById('location').value;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=c7911001fbb406aa98e26c70ab98ec78`, { mode: 'cors' })
+      .then((response) => (response.json()))
+      .then((response) => {
+        const { description } = response.weather[0];
+        const mainTemp = Math.round((response.main.temp) - 273);
+        const cityName = response.name;
+        const countryName = response.sys.country;
+        const { humidity } = response.main;
+        const { pressure } = response.main;
+        const lowTemp = Math.round((response.main.temp_min) - 273);
+        const highTemp = Math.round((response.main.temp_max) - 273);
 
-            // convert to fahrenheit
-            
-            const ferhButton = document.getElementById('fer');
-            ferhButton.onclick = () => {
-               
-                const mainTempFer = (mainTemp * 9/5) + 32;
-                const lowTempFer = (lowTemp * 9/5) + 32;
-                const highTempFer = (highTemp * 9/5) + 32;
-                weatherCard(mainTempFer, cityName, countryName, lowTempFer, highTempFer, humidity, pressure, description);
-               
-            }
-    
+        weatherCard(mainTemp, cityName, countryName, lowTemp,
+          highTemp, humidity, pressure, description);
 
-        });
-        
-    }
-}
+        // convert to fahrenheit
+
+        const ferhButton = document.getElementById('fer');
+        ferhButton.onclick = () => {
+          const mainTempFer = Math.round((mainTemp * (9 / 5)) + 32);
+          const lowTempFer = Math.round((lowTemp * (9 / 5)) + 32);
+          const highTempFer = Math.round((highTemp * (9 / 5)) + 32);
+          weatherCard(mainTempFer, cityName, countryName, lowTempFer,
+            highTempFer, humidity, pressure, description);
+        };
+      });
+  };
+};
 
 
 const getData = async () => {
-    try {
-        const weather = await dataInfo();
-    } catch (err) {
-        return err;
-    }
-}
+  try {
+    const weather = await dataInfo();
+    return weather;
+  } catch (err) {
+    return err;
+  }
+};
 
 export default getData;
