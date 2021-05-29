0e77cfbf7920e702c732b7654205367106cb56a6
george-shammar <george.gbenle@outlook.com>
2021-05-29 14:08:55 +0000

    Clean up code

--- src/dom.js
+++ src/dom.js
@@ -109,50 +109,13 @@ const weatherCard = () => {
 
     // clouds
     const cloudContainer = document.createElement('div');
-    cityContainer.appendChild(cloudContainer);
+    
 
     const cloudParagraph = document.createElement('p');
     cloudParagraph.setAttribute('class', 'main ps-5');
     const cloud = document.createTextNode('Clouds');
     cloudParagraph.appendChild(cloud);
     cloudContainer.appendChild(cloudParagraph);
+
+    cityContainer.appendChild(cloudContainer);
 }
-
-
-
-
-
-        <div class="weather-card mt-5 border shadow"> card Container
-            <i class="fas fa-cloud-sun-rain cloud"></i>
-
-            <div class="d-flex justify-content-center">
-                <div class="pe-5">
-                    <p class="main">29</p>
-                    <p class="fs-3">Accra, GH</p>
-                </div>
-                <div class="line"></div>
-                <div>
-                    <p class="main ps-5">Clouds</p>
-                </div>
-            </div>
-            {/* <div class="d-flex justify-content-center color-font"> dataContainer
-                <div> maxTempContainer
-                    <h4 class="px-3 fw-bold">Max-Temp</h4>
-                    <h4 class="px-5 fw-bold">20</h4>
-                </div>
-                <div>
-                    <h4 class="px-3 fw-bold">Min-Temp</h4>
-                    <h4 class="px-5 fw-bold">20</h4>
-                </div>
-                <div>
-                    <h4 class="px-3 fw-bold">Humidity</h4>
-                    <h4 class="px-5 fw-bold">20</h4>
-                </div>
-                <div>
-                    <h4 class="px-3 fw-bold">Pressure</h4>
-                    <h4 class="px-5 fw-bold">1012ha</h4>
-                </div>
-            </div> */}
-           
-           
-        </div>
