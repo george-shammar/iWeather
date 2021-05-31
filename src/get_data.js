a63363aa56316839caf3d08ec129e27038ff1cde
george-shammar <george.gbenle@outlook.com>
2021-05-31 21:34:12 +0000

    Add toggle box with style

--- src/get_data.js
+++ src/get_data.js
@@ -9,33 +9,16 @@ const dataInfo = (location) => {
       .then((response) => (response.json()))
       .then((response) => {
         const { description } = response.weather[0];
-        const mainTemp = Math.round((response.main.temp) - 273);
+        const mainTemp = response.main.temp;
         const cityName = response.name;
         const countryName = response.sys.country;
         const { humidity } = response.main;
         const { pressure } = response.main;
-        const lowTemp = Math.round((response.main.temp_min) - 273);
-        const highTemp = Math.round((response.main.temp_max) - 273);
+        const lowTemp = response.main.temp_min;
+        const highTemp = response.main.temp_max;
 
         weatherCard(mainTemp, cityName, countryName, lowTemp,
           highTemp, humidity, pressure, description);
-
-        // convert to fahrenheit
-
-        const ferhButton = document.getElementById('fer');
-        ferhButton.onclick = () => {
-          const mainTempFer = Math.round((mainTemp * (9 / 5)) + 32);
-          const lowTempFer = Math.round((lowTemp * (9 / 5)) + 32);
-          const highTempFer = Math.round((highTemp * (9 / 5)) + 32);
-          weatherCard(mainTempFer, cityName, countryName, lowTempFer,
-            highTempFer, humidity, pressure, description);
-        };
-        // convert to celcius
-        const celButton = document.getElementById('cel');
-        celButton.onclick = {
-          weatherCard(mainTemp, cityName, countryName, lowTemp,
-            highTemp, humidity, pressure, description);
-        }
 
 
       }).catch((err) => {
@@ -75,3 +58,20 @@ const getData = async () => {
 };
 
 export default getData;
+
+ // // convert to fahrenheit
+
+        // const ferhButton = document.getElementById('fer');
+        // ferhButton.onclick = () => {
+        //   const mainTempFer = Math.round((mainTemp * (9 / 5)) + 32);
+        //   const lowTempFer = Math.round((lowTemp * (9 / 5)) + 32);
+        //   const highTempFer = Math.round((highTemp * (9 / 5)) + 32);
+        //   weatherCard(mainTempFer, cityName, countryName, lowTempFer,
+        //     highTempFer, humidity, pressure, description);
+        // };
+        // // convert to celcius
+        // const celButton = document.getElementById('cel');
+        // celButton.onclick = {
+        //   weatherCard(mainTemp, cityName, countryName, lowTemp,
+        //     highTemp, humidity, pressure, description);
+        // }
